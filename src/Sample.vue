7ff0e792152aad27381bb1e419a441670fe9cae6
yKazihara <run5150@gmail.com>
2025-03-30 19:25:39 +0900

    変数を埋め込み表示するサンプル。

--- src/Sample.vue
+++ src/Sample.vue
@@ -1,13 +1,18 @@
 <script setup>
 const title = 'Nyan programer'
+let number = 9999
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
+  <h2>Number: {{ number }}</h2>
 </template>
 
 <style>
 h1 {
   color: green;
+}
+h2 {
+  color: red;
 }
 </style>
