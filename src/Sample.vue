9aa271028b0d59a32a7378867657ee92f485ac49
yKazihara <run5150@gmail.com>
2025-03-31 07:49:21 +0900

    ボタン配置のサンプル。

--- src/Sample.vue
+++ src/Sample.vue
@@ -1,11 +1,15 @@
 <script setup>
 const title = 'Nyan programer'
 let number = 9999
+function increment() {
+  console.log('クリック！');
+}
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Number: {{ number - 1000 }}</h2>
+  <button @click="increment">ボタン</button>
 </template>
 
 <style>
