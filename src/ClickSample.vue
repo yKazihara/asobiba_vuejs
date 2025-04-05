22343e75e9995306d087d0813597bdd47ddd9732
yKazihara <run5150@gmail.com>
2025-04-06 07:30:41 +0900

    イベント修飾子のサンプル。

--- src/ClickSample.vue
+++ src/ClickSample.vue
@@ -2,6 +2,7 @@
 import { ref, reactive } from 'vue'
 
 const count = ref(0)
+const count2 = ref(0)
 function countUp() {
   count.value++
 }
@@ -11,6 +12,11 @@ function countUp() {
   <p>{{ count }}</p>
   <button @click="count++">button</button>
   <button @click="countUp">button</button>
+
+  <p>{{ count2 }}</p>
+  <div @click="count2++">
+    <button @click.stop="count2 = 50">ボタン</button>
+  </div>
 </template>
 
 <style>
