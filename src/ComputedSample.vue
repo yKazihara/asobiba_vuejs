ef436c80365c05d032f90a5ed4718df5c3fc7bd7
yKazihara <run5150@gmail.com>
2025-04-07 07:05:36 +0900

    computedのサンプル。

---
+++ src/ComputedSample.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const score = ref(0)
+const evaluation = computed(() => {score.value > 3 ? 'Good!!!👍' : 'Bad👎'})
+console.log(evaluation);
+</script>
+
+<template>
+  <p>{{ score > 3 ? 'Good!!!👍' : 'Bad👎' }}</p>
+  <p>{{ evaluation }}</p>
+  <p>{{ score }}</p>
+  <button @click="score++">+1</button>
+</template>
+
+<style>
+h1 {
+  color: green;
+}
+h2 {
+  color: red;
+}
+</style>
---
+++ src/computed_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './ComputedSample.vue'
+
+createApp(App).mount('#app')
