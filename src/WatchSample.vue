bcf5f532f654bd246ff98e547352515d5ef108fa
yKazihara <run5150@gmail.com>
2025-04-08 07:17:30 +0900

    watchEffect使用法のサンプル。

---
+++ src/WatchSample.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const count = ref(0)
+watchEffect(() => {
+  console.log('watchEffect')
+  console.log(count.value)
+})
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <button @click="count++">+1</button>
+</template>
+
+<style>
+h1 {
+  color: green;
+}
+h2 {
+  color: red;
+}
+</style>
