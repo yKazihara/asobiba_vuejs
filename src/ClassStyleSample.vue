0fcf0c4484f6f22fc30317f92686a13b9ed0289a
yKazihara <run5150@gmail.com>
2025-04-09 07:22:47 +0900

    class属性や:style属性のサンプル。

---
+++ src/ClassStyleSample.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+const isPink = ref(true)
+const isBgGreen = ref(true)
+const className = ref('pink')
+function toggleClass() {
+  isPink.value = !isPink.value
+  isBgGreen.value = !isBgGreen.value
+}
+</script>
+
+<template>
+  <div class="pink bg-green">Nyan</div>
+  <div :class="'pink bg-green'">Nyan</div>
+  <div :class="{pink: true, 'bg-green':true}">Nyan</div>
+  <div :class="{ pink: true, 'bg-green': false }">Nyan</div>
+
+  <div :class="[className, { 'bg-green': isBgGreen }]">Nyan</div>
+  <button @click="toggleClass">Toggle</button>
+
+  <div class="border" :class="[className, { 'bg-green': isBgGreen }]">Nyan</div>
+</template>
+
+<style>
+.pink {
+  color: pink;
+}
+.bg-green {
+  background-color: green;
+}
+.border {
+  border: 2px dotted black;
+}
+</style>
---
+++ src/class_style_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './ClassStyleSample.vue'
+
+createApp(App).mount('#app')
