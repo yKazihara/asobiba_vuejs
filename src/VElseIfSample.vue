1a518622317e01d27343781067b455c87653da70
yKazihara <run5150@gmail.com>
2025-04-10 07:02:28 +0900

    v-if,v-else,v-else-ifのサンプル。

---
+++ src/VElseIfSample.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const ok = ref(true)
+const maybeOk = ref(true)
+</script>
+
+<template>
+  <button @click="ok = !ok">toggle</button>
+  <template v-if="ok">
+    <p>OK!</p>
+    <p>Hello!</p>
+    <p>Nyan!</p>
+    <p>Nyan Nyan!</p>
+  </template>
+</template>
+
+<style>
+
+</style>
---
+++ src/v-else-if_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './VElseIfSample.vue'
+
+createApp(App).mount('#app')
