79794542f1f59ca321fdaa9d1057b92859615481
yKazihara <run5150@gmail.com>
2025-04-11 07:13:39 +0900

    v-showのサンプル。

---
+++ src/VShowSample.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+const ok = ref(true)
+</script>
+
+<template>
+  <button @click="ok = !ok">toggle</button>
+  <p v-show="ok">OK!</p>
+  <p v-show="!ok">NO!</p>
+</template>
+
+<style>
+
+</style>
