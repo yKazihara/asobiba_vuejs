143f1ba1ba3aa86482ca42a1d612885ac4e5aeb5
yKazihara <run5150@gmail.com>
2025-04-13 16:49:46 +0900

    コンポーネントのサンプル。

---
+++ src/ComponentSample.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CountUp from './components/CountUpSample.vue'
+</script>
+
+<template>
+  <h1>Component</h1>
+  <CountUp />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/CountUpSample.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div><h2>CountUp++</h2></div>
+  <p>count:{{ count }}</p>
+  <button @click="count++">+1</button>
+</template>
+
+<style>
+
+</style>
