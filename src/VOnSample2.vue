2f034792bf5f569c446328176c8040b529e6c0ea
yKazihara <run5150@gmail.com>
2025-04-16 07:18:57 +0900

    v-onのサンプル。

---
+++ src/VOnSample2.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style>
+
+</style>
---
+++ src/v-on_sample_2.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './VOnSample2.vue'
+
+createApp(App).mount('#app')
