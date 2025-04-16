8370196df8f72e6277f4575ab213a120b6e746ee
yKazihara <run5150@gmail.com>
2025-04-17 07:05:58 +0900

    v-modelのサンプル。

---
+++ src/VModelSample.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
+
+<template>
+  <input v-model="text" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
+
+<style>
+
+</style>
---
+++ src/v-model_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './VModelSample.vue'
+
+createApp(App).mount('#app')
