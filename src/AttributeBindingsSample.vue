24d80a4465dde581e2b911617ffc24500ff4af04
yKazihara <run5150@gmail.com>
2025-04-29 07:23:02 +0900

    Attribute Bindingsのサンプル。

---
+++ src/AttributeBindingsSample.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+const isRed = ref(true)
+const color = ref('green')
+
+function toggleRed() {
+  isRed.value = !isRed.value
+}
+
+function toggleColor() {
+  color.value = color.value === 'green' ? 'blue' : 'green'
+}
+</script>
+
+<template>
+  <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+  </p>
+
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
---
+++ src/attribute_bindings_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './AttributeBindingsSample.vue'
+
+createApp(App).mount('#app')
