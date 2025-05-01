36382ff6bbad3349e145b9fc6e05f7170dfabc58
yKazihara <run5150@gmail.com>
2025-05-02 07:08:39 +0900

    Simple Componentのサンプル。

---
+++ src/SimpleComponentSample.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import TodoItem from '@/components/TodoItem.vue'
+
+const groceryList = ref([
+  { id: 0, text: 'Vegetables' },
+  { id: 1, text: 'Cheese' },
+  { id: 2, text: 'Whatever else humans are supposed to eat' }
+])
+</script>
+
+<template>
+  <ol>
+    <TodoItem
+        v-for="item in groceryList"
+        :todo="item"
+        :key="item.id"
+    ></TodoItem>
+  </ol>
+</template>
+
+<style>
+
+</style>
