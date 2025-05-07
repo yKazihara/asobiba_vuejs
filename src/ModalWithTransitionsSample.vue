d3350eea19d273254ac547e5b40ad5086c4712bd
yKazihara <run5150@gmail.com>
2025-05-08 07:23:48 +0900

    Modal with Transitionsのサンプル。

---
+++ src/ModalWithTransitionsSample.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Modal from '@/components/ModalSample.vue'
+import { ref } from 'vue'
+
+const showModal = ref(false)
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>Custom Header</h3>
+      </template>
+    </modal>
+  </Teleport>
+</template>
+
+<style>
+
+</style>
---
+++ src/modal_with_transitions_sample.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './ModalWithTransitionsSample.vue'
+
+createApp(App).mount('#app')
